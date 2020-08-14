9027b9f04df5c2f4921b229c234d1264bfd4323c
wenny mei <383776758.@qq.com>
2020-08-14 22:02:20 +0800

    管理页框架

---
+++ js/index.js
@@ -0,0 +1,52 @@
+let startX, startWidth;
+
+const getScalableDivWidth = () => {
+  return startWidth = parseInt($(".scalable").width(), 10);
+};
+
+startWidth = localStorage.getItem("scalableWidth") || getScalableDivWidth();
+
+let scalable = $(".scalable");
+let separator = $(".separator");
+let body = $("html");
+
+$(".scalable").width(startWidth);
+
+const onDrag = (e) => {
+  separator.css("cursor", "col-resize");
+  scalable.width(startWidth + e.clientX - startX);
+};
+
+const stopDrag = () => {
+  separator.css("cursor", "");
+  localStorage.setItem("scalableWidth", getScalableDivWidth())
+  body.off("mousemove", onDrag);
+  body.off("mouseup", stopDrag);
+};
+
+const startDrag = (e) => {
+  startX = e.clientX;
+  startWidth = getScalableDivWidth();
+  body.on("mousemove", onDrag);
+  body.on("mouseup", stopDrag);
+};
+
+separator.on("mousedown", startDrag);
+
+// 分页
+// @author: huimei
+$('.main-interview').hide();
+$('.main-enroll').show();
+
+$('.enroll-card').click(function() {
+  $('.enroll-card').addClass('leftcard');
+  $('.interview-card').removeClass('leftcard');
+  $('.main-enroll').show();
+  $('.main-interview').hide();
+})
+$('.interview-card').click(function() {
+  $('.enroll-card').removeClass('leftcard');
+  $('.interview-card').addClass('leftcard');
+  $('.main-enroll').hide();
+  $('.main-interview').show();
+})
