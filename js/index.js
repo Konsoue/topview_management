91d6f3f569720dbda2abff150572f1886655cfe7
87YLu <1790599287@qq.com>
2020-08-20 12:49:45 +0800

    优化部分代码，新增报名界面选择功能

--- js/index.js
+++ js/index.js
@@ -1,47 +1,35 @@
-let startX, startWidth;
+/* 左侧栏拖动 */
+/* @author: 陈泳充 */
+$(".scalable").width(localStorage.getItem("scalableWidth") || parseInt($(".scalable").width(), 10));
 
-const getScalableDivWidth = () => {
-  return startWidth = parseInt($(".scalable").width(), 10);
-};
+$(".separator").on("mousedown", function (e) {
+  let startX = e.clientX;
+  let startWidth = parseInt($(".scalable").width(), 10);
 
-startWidth = localStorage.getItem("scalableWidth") || getScalableDivWidth();
+  function onDrag(e) {
+    $("html").css("cursor", "col-resize");
+    $(".scalable").width(startWidth + e.clientX - startX);
+  }
 
-let scalable = $(".scalable");
-let separator = $(".separator");
-let body = $("html");
+  $("html").on("mousemove", onDrag);
 
-$(".scalable").width(startWidth);
+  $("html").one("mouseup", function () {
+    $("html").css("cursor", "");
+    localStorage.setItem("scalableWidth", parseInt($(".scalable").width(), 10))
+    $("html").off("mousemove", onDrag);
+  });
+});
 
-const onDrag = (e) => {
-  separator.css("cursor", "col-resize");
-  scalable.width(startWidth + e.clientX - startX);
-};
-
-const stopDrag = () => {
-  separator.css("cursor", "");
-  localStorage.setItem("scalableWidth", getScalableDivWidth())
-  body.off("mousemove", onDrag);
-  body.off("mouseup", stopDrag);
-};
-
-const startDrag = (e) => {
-  startX = e.clientX;
-  startWidth = getScalableDivWidth();
-  body.on("mousemove", onDrag);
-  body.on("mouseup", stopDrag);
-};
-
-separator.on("mousedown", startDrag);
 
 // 分页
 // @author: huimei
-$('.enroll-card').click(function() {
+$('.enroll-card').click(function () {
   $('.enroll-card').addClass('leftcard');
   $('.interview-card').removeClass('leftcard');
   $('.main-enroll').show();
   $('.main-interview').hide();
 })
-$('.interview-card').click(function() {
+$('.interview-card').click(function () {
   $('.enroll-card').removeClass('leftcard');
   $('.interview-card').addClass('leftcard');
   $('.main-enroll').hide();
@@ -52,14 +40,14 @@ $('.interview-card').click(function() {
  *@author:   huimei
  *@function: 控制登录框
  *@params:   wu
-*/
+ */
 $('.login-page').hide();
-$('.avatar').click(function() {
+$('.avatar').click(function () {
   $('.login-page').slideToggle()
 })
 
-$('.login-page').mousedown(function(e) {
-  
+$('.login-page').mousedown(function (e) {
+
   // e.pageX
   var positionDiv = $('.login-page').offset();
   var distenceX = e.pageX - positionDiv.left;
@@ -67,33 +55,33 @@ $('.login-page').mousedown(function(e) {
   //alert(distenceX)
   // alert(positionDiv.left);
 
-  $(document).mousemove(function(e) {
-      $('.login-page').css('cursor','pointer');
-      var x = e.pageX - distenceX;
-      var y = e.pageY - distenceY;
+  $(document).mousemove(function (e) {
+    $('.login-page').css('cursor', 'pointer');
+    var x = e.pageX - distenceX;
+    var y = e.pageY - distenceY;
 
-      if (x < 0) {
-          x = 0;
-      } else if (x > $(document).width() - $('.login-page').outerWidth(true)) {
-          x = $(document).width() - $('.login-page').outerWidth(true);
-      }
+    if (x < 0) {
+      x = 0;
+    } else if (x > $(document).width() - $('.login-page').outerWidth(true)) {
+      x = $(document).width() - $('.login-page').outerWidth(true);
+    }
 
-      if (y < 0) {
-          y = 0;
-      } else if (y > $(document).height() - $('.login-page').outerHeight(true)) {
-          y = $(document).height() - $('.login-page').outerHeight(true);
-      }
+    if (y < 0) {
+      y = 0;
+    } else if (y > $(document).height() - $('.login-page').outerHeight(true)) {
+      y = $(document).height() - $('.login-page').outerHeight(true);
+    }
 
-      if(distenceX < ($(".loginBox").position().left) || distenceY < ($(".loginBox").position().top) ) {
-        $('.login-page').css({
-          'left': x + 'px',
-          'top': y + 'px'
-        });
-      }
+    if (distenceX < ($(".loginBox").position().left) || distenceY < ($(".loginBox").position().top)) {
+      $('.login-page').css({
+        'left': x + 'px',
+        'top': y + 'px'
+      });
+    }
   });
 
-  $(document).mouseup(function() {
-      $(document).off('mousemove');
-      $('.login-page').css('cursor','default');
+  $(document).mouseup(function () {
+    $(document).off('mousemove');
+    $('.login-page').css('cursor', 'default');
   });
 });
--- js/students_mes.js
+++ js/students_mes.js
@@ -13,17 +13,77 @@ $('.students thead tr').on('mousedown', '.move-loc', function (e) {
   let index = that.parent().index();
   let studentsStartX = e.clientX;
   let studentsStartWidth = parseInt(that.parent().width(), 10);
-  $("html").on("mousemove", function (e) {
+
+  function studentsDrag(e) {
     $(this).css("cursor", "col-resize");
     let result = studentsStartWidth + e.clientX - studentsStartX;
     result = result < 30 ? 30 : result;
     result = result > 400 ? 400 : result;
     that.parent().width(result);
-  });
+  }
+  $("html").on("mousemove", studentsDrag);
+
   $("html").one("mouseup", function () {
     $(this).css("cursor", "");
-    $("html").off("mousemove");
+    $("html").off("mousemove", studentsDrag);
     widthArr[index] = that.parent().width();
     localStorage.setItem("studentsWidth", JSON.stringify(widthArr));
   });
+})
+
+/* 学生信息全选 */
+/* @author: 陈泳充 */
+let [ctrlSure, shiftSure] = [0, 0];
+let checkArr = [];
+let shiftFirst = null;
+
+function getKeyCode(e) {
+  ctrlSure = e.keyCode === 17 ? 1 : 0;
+  shiftSure = e.keyCode === 16 ? 1 : 0;
+}
+
+$("html").one("keydown", getKeyCode);
+$("html").on('keyup', function () {
+  $("html").one("keydown", getKeyCode);
+  [ctrlSure, shiftSure] = [0, 0];
+  shiftFirst = null;
+})
+
+$('.main-enroll .students tbody').on("click", "tr", function () {
+  if (ctrlSure) {
+    $(this).toggleClass("checked");
+    let index = checkArr.indexOf($(this).index());
+    index === -1 ? checkArr.push($(this).index()) : checkArr.splice(index, 1);
+  } else if (shiftSure) {
+    if (checkArr.length !== 0) {
+      shiftFirst = shiftFirst !== null ? shiftFirst : checkArr[checkArr.length - 1];
+      $('.main-enroll .students tbody tr').removeClass('checked');
+      let [first, last] = [shiftFirst, $(this).index()];
+      if (last < first) {
+        first = [last, last = first][0];
+      }
+      checkArr = [];
+      for (let i = first; i <= last; i++) {
+        $('.main-enroll .students tbody tr').eq(i).addClass('checked');
+        checkArr.push(i);
+      }
+    }
+  } else {
+    if (checkArr.length > 1) {
+      $(this).siblings().removeClass('checked');
+      $(this).addClass("checked");
+      checkArr = [$(this).index()];
+    } else {
+      $(this).siblings().removeClass('checked');
+      $(this).toggleClass("checked");
+      checkArr = checkArr.indexOf($(this).index()) === -1 ? [$(this).index()] : [];
+    }
+  }
+})
+
+
+/* 查看学生其他信息 */
+$('.main-enroll .students tbody').on("click", "tr a", function (e) {
+  e.stopPropagation()
+  // 等接口
 })
